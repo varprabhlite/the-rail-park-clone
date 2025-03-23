44e79d2d5e7799a257979fdbbacd37b238e232bb
varprabhlite <185780179+varprabhlite@users.noreply.github.com>
2025-03-24 01:18:26 +0530

    fix regex

--- script.js
+++ script.js
@@ -141,7 +141,7 @@ emailInput.addEventListener("input", () => {
 });
 
 function validateEmail(email) {
-  const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
   return emailRegex.test(email.toLowerCase());
 }
 
